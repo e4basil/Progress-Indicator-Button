0e52f230fee33b63ed3916eac0709553b0a06654
GitHub Action <action@github.com>
2021-05-25 05:22:12 +0000

    Template cleanup

--- buildSrc/src/main/kotlin/publish.gradle.kts
+++ buildSrc/src/main/kotlin/publish.gradle.kts
@@ -93,7 +93,7 @@ afterEvaluate {
                         version = "$version-SNAPSHOT"
                     }
                     description.set("A template for Kotlin Android projects")
-                    url.set("https://github.com/cortinico/kotlin-android-template/")
+                    url.set("https://github.com/e4basil/progressindicatorbutton/")
 
                     licenses {
                         license {
@@ -103,18 +103,18 @@ afterEvaluate {
                     }
                     developers {
                         developer {
-                            id.set("cortinico")
-                            name.set("Nicola Corti")
+                            id.set("e4basil")
+                            name.set("e4basil")
                         }
                     }
                     scm {
-                        connection.set("scm:git:git://github.com/cortinico/kotlin-android-template.git")
-                        developerConnection.set("scm:git:ssh://github.com/cortinico/kotlin-android-template.git")
-                        url.set("https://github.com/cortinico/kotlin-android-template/")
+                        connection.set("scm:git:git://github.com/e4basil/progressindicatorbutton.git")
+                        developerConnection.set("scm:git:ssh://github.com/e4basil/progressindicatorbutton.git")
+                        url.set("https://github.com/e4basil/progressindicatorbutton/")
                     }
                     issueManagement {
                         system.set("GitHub Issues")
-                        url.set("https://github.com/cortinico/kotlin-android-template/issues")
+                        url.set("https://github.com/e4basil/progressindicatorbutton/issues")
                     }
                 }
             }
